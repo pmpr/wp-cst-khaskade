62b1489141c2d0866f5f7ba40ccc3bcf9337a946
pmpr.dev <pmpr.dev@gmail.com>
2026-02-21 12:53:11 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             69999f63b7b19             |
+    |             6999aa8fb98cf             |
     |_______________________________________|
 */
  use Pmpr\Custom\Khaskade\Khaskade; Khaskade::symcgieuakksimmu();
--- src/Pmpr/Custom/Khaskade/Khaskade.php
+++ src/Pmpr/Custom/Khaskade/Khaskade.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             69999f63b7b19             |
+    |             6999aa8fb98cf             |
     |_______________________________________|
 */
  namespace Pmpr\Custom\Khaskade; use Pmpr\Common\Foundation\Container\ComponentInitiator; use Pmpr\Common\Foundation\Interfaces\Constants; use Pmpr\Custom\Khaskade\OrderCampaign\OrderCampaign; class Khaskade extends ComponentInitiator { const PREFIX = 'cst_khaskade_'; public function register() { $this->gkieogwukagigisy(__DIR__, [Constants::qescuiwgsyuikume => static function () { return __('Khaskade Custom', PR__CST__KHASKADE); }, Constants::sguyaymiiiiewame => Setting::class]); } public function aqyikqugcomoqqqi() { if ($this->caokeucsksukesyo()->cqusmgskowmesgcg()->iqqgmieeqemiowuk('sms')) { OrderCampaign::symcgieuakksimmu(); } } }
